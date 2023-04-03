64f83c796e0ef4f36ea9c0953db95b072f2a8c98
LazlowHUN <doczilaci1111@gmail.com>
2023-04-03 19:01:13 +0200

    5th homework fixed

--- HAZI/HAZI05/HAZI05.py
+++ HAZI/HAZI05/HAZI05.py
@@ -1,45 +1,80 @@
+import numpy as np
 import pandas as pd
-from sklearn.metrics import accuracy_score, confusion_matrix
+import seaborn as sns
+from typing import Tuple
+from scipy.stats import mode
+from sklearn.metrics import confusion_matrix
 import math
 
+csv_path = "diabetes.csv"
 
 class KNNClassifier:
-    def __init__(self, k):
+
+    #Constuctor
+    def __init__(self,k:int,test_split_ratio:float):
         self.k = k
-        self.X_train = None
+        self.test_split_ratio = test_split_ratio
+        self.x_train = None
         self.y_train = None
+        self.x_test = None
+        self.y_test = None
         self.y_preds = None
 
-    def fit(self, X_train, y_train):
-        self.X_train = X_train
-        self.y_train = y_train
-
-    def euclidean_distance(self, row1, row2):
-        distance = 0.0
-        for i in range(len(row1)):
-            distance += (row1[i] - row2[i]) ** 2
-        return math.sqrt(distance)
-
-    def predict(self, X_test):
-        distances = []
-        targets = []
-        for i in range(len(self.X_train)):
-            distance = self.euclidean_distance(self.X_train.iloc[i], X_test)
-            distances.append([distance, i])
-        distances.sort()
-        for i in range(self.k):
-            index = distances[i][1]
-            targets.append(self.y_train.iloc[index])
-        self.y_preds = pd.Series(targets)
-
-    @staticmethod
-    def accuracy(y_test, y_preds):
-        return accuracy_score(y_test, y_preds)
-
-    @staticmethod
-    def confusion_matrix(y_test, y_preds):
-        return confusion_matrix(y_test, y_preds)
-        
     @property
     def k_neighbors(self):
         return self.k
+    
+    @staticmethod 
+    def load_csv(csv_path:str) ->Tuple[pd.DataFrame,pd.Series]:
+        np.random.seed(42)
+        dataset = pd.read_csv(csv_path, header=None)
+        dataset = dataset.sample(frac=1, random_state=42).reset_index(drop=True)
+        x,y = dataset.iloc[:,:4],dataset.iloc[:,-1]
+        return x,y
+    
+    #Train test split try optim
+    def train_test_split(self,features:pd.DataFrame,labels:pd.Series) -> None:
+        test_size = int(len(features)*self.test_split_ratio)
+        train_size = len(features) - test_size
+        assert len(features) == test_size + train_size, 'Size mismatch!'
+
+        self.x_train = features.iloc[:train_size,:]
+        self.y_train = labels.iloc[:train_size]
+
+        self.x_test = features.iloc[train_size:train_size+test_size,:]
+        self.y_test = labels.iloc[train_size:train_size+test_size]
+
+    def euclidean(self, element_of_x: pd.Series) -> pd.Series:
+        return pd.Series((self.x_train - element_of_x).pow(2).sum(axis=1)).pow(1/2)
+
+    def predict(self, x_test: pd.DataFrame) -> pd.Series:
+        labels_pred = []
+        for i, x_test_element in x_test.iterrows():
+            distances = self.euclidean(x_test_element)
+            distances = pd.DataFrame({'distance': distances, 'label': self.y_train})
+            distances = distances.sort_values('distance')
+            label_pred = mode(distances.iloc[:self.k,1])[0][0]
+            labels_pred.append(label_pred)
+        self.y_preds = pd.Series(labels_pred)
+
+    def accuracy(self) -> float:
+        true_positive = (self.y_test == self.y_preds).sum()
+        return true_positive / len(self.y_test) * 100
+    
+    def confusion_matrix(self) -> np.ndarray:
+        conf_matrix = pd.crosstab(self.y_test, self.y_preds)
+        return conf_matrix
+
+    def best_k(csv_path:str, test_split_ratio:float) -> Tuple[int,float]:
+        best_k = 0
+        best_acc = 0.0
+        for k in range(1,21):
+            knn = KNNClassifier(k=k,test_split_ratio=test_split_ratio)
+            x,y = knn.load_csv(csv_path)
+            knn.train_test_split(x,y)
+            knn.predict(knn.x_test)
+            acc = round(knn.accuracy(),2)
+            if(acc > best_acc):
+                best_k = k
+                best_acc = acc
+        return (best_k,best_acc)
