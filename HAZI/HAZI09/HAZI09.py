8dee83d9a607e173cc8e6b5a34f1dcb472e5ccae
LazlowHUN <doczilaci1111@gmail.com>
2023-04-30 20:18:57 +0200

    9th homework finished

--- HAZI/HAZI09/HAZI09.py
+++ HAZI/HAZI09/HAZI09.py
@@ -1,38 +1,32 @@
-import matplotlib.pyplot as plt
-import seaborn as sns
-sns.set()
 import numpy as np
-import sklearn.datasets as dt
+from sklearn.datasets import load_digits
 from sklearn.cluster import KMeans
-from sklearn.metrics import accuracy_score
-from scipy.stats import mode
 from sklearn.metrics import confusion_matrix
 
-class KMeansOnDigits():
+class KMeansOnDigits:
 
     def __init__(self, n_clusters, random_state):
         self.n_clusters = n_clusters
-        self.random_state = random_state
+        self.random_state=random_state
 
     def load_dataset(self):
-        self.digits = dt.load_digits()
+        self.digits = load_digits()
 
     def predict(self):
-        model = KMeans(n_clusters=self.n_clusters, random_state=self.random_state)
-        clusters = model.fit_predict(self.digits)
-        self.clusters = clusters
+        self.model = KMeans(n_clusters=self.n_clusters, random_state=self.random_state)
+        self.clusters = self.model.fit_predict(self.digits.data)
 
     def get_labels(self):
-        labels = np.zeros_like(self.clusters)
-        for cluster in range(10):
-            mask = (self.clusters == cluster)
-            subarray = self.digits.target[mask]
-            mode = np.bincount(subarray).argmax()
-            labels[mask] = mode
-        self.labels = labels
-    
+        l = np.zeros_like(self.clusters)
+        for c in range(self.n_clusters):
+            mask = (self.clusters == c)
+            target = self.digits.target[mask]
+            mode = np.bincount(target).argmax()
+            l[mask] = mode
+        self.labels = l
+
     def calc_accuracy(self):
-        self.accuracy = round(accuracy_score(self.digits.target, self.labels), 2)
+        self.accuracy = round(np.mean(self.labels == self.digits.target), 2)
 
     def confusion_matrix(self):
-        self.math = confusion_matrix(self.digits.target, self.labels)
+        self.mat = confusion_matrix(self.digits.target, self.labels)
